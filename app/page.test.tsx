5daa0ae2610c837ed8afd1d3a9e402e8132bc036
hiroshi <hiroshi3110@gmail.com>
2024-01-03 17:26:49 +0900

    Restore deleted lines from memory so that the page test works.

--- app/page.test.tsx
+++ app/page.test.tsx
@@ -7,13 +7,19 @@ import Page from './page';
 describe('Page', () => {
   it('renders a topic', async () => {
     global.fetch = jest.fn((url, opts) => {
-      console.log(url, opts);
+      // console.log(url, opts);
       return new Promise((resolve, reject) => {
         if (url === '/api/topics') {
           return resolve({json: () => new Promise((resolve) => resolve([{_id: '0'}]))});
         }
       });
-    });
+    }) as jest.Mock;
+
+    render(<Page/>);
+    // screen.debug();
+    await waitFor(() => screen.findByRole('listitem'));
+    // screen.debug();
+
 
     // const heading = screen.getByRole('heading', { level: 1 })
     // expect(heading).toBeInTheDocument()
