3758794cabdaf487117cbd399159fe6caa5eef67
hiroshi <hiroshi3110@gmail.com>
2024-01-29 16:30:08 +0900

    Make negative labels can be saved.

--- lib/label.ts
+++ lib/label.ts
@@ -1,12 +1,16 @@
 function queryToLabels(queryString) {
-  return queryString.split(/\s+/).map((pair) => {
-    const [k, v] = pair.split(':');
-    return {k, v};
+  return queryString.split(/\s+/).map((l) => {
+    const n = l[0] === '-';
+    const label = n ? l.slice(1) : l;
+    const [k, v] = label.split(':');
+    return n ? {k, v, n} : {k, v};
   });
 }
 
 function labelsToQuery(labels) {
-  return labels.map(({k,v}) => `${k}:${v}`).join(' ')
+  return labels.map(({k, v, n}) => {
+    return n ? `-${k}:${v}` : `${k}:${v}`;
+  }).join(' ');
 }
 
 export { queryToLabels, labelsToQuery };
